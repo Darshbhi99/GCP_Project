bea31bc7147c2545df1f2c2e06efbd90e7e13f72
Darshan Bhiwapurkar <darshan@Darshans-MacBook-Air.local>
2024-01-04 14:46:39 +0530

    Adding and pulling files from GCP Bucket

--- GCloud&Bash_Basics/gcloud-bash.sh
+++ GCloud&Bash_Basics/gcloud-bash.sh
@@ -4,20 +4,23 @@ gcloud auth login
 #Set Default project-id
 gcloud config set project {PROJECT_ID}
 
-# Test your config settings
+#Test your config settings
 gcloud config list
 
-# List of all the projects in your GCP Account
+#List of all the projects in your GCP Account
 gcloud projects list
 
-# Get Current project
+#Get Current project
 gcloud config get-value project
 
-# Save the result of the above cd in a variable
-export PROJECT_ID=$(cloud config get-value project) 
+#Save the result of the above cd in a variable
+export PROJECT_ID=$(cloud config get-value project)
+
+#Gives the output of above command
+echo $PROJECT_ID
 
 #Create a new variable called BUCKET NAME
-export BUCKET_NAME="$(PROJECT_ID}-test-bucket"
+export BUCKET_NAME="${PROJECT_ID}-test-bucket"
 
-# Create a New GCP Storage Bucket
+#Create a New GCP Storage Bucket
 gsutil mb -p ${PROJECT_ID} gs://${BUCKET_NAME}
